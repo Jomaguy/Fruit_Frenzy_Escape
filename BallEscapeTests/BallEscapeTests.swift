c82f17336eab61ca2ad1ef8fc4859a5985b58ed2
Jonathan Mahrt Guyou <jonathanmahrtguyou@gmail.com>
2024-11-12 19:11:15 -0500

    Initial Commit

---
+++ BallEscapeTests/BallEscapeTests.swift
@@ -0,0 +1,17 @@
+//
+//  BallEscapeTests.swift
+//  BallEscapeTests
+//
+//  Created by Jonathan Mahrt Guyou on 11/12/24.
+//
+
+import Testing
+@testable import BallEscape
+
+struct BallEscapeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
